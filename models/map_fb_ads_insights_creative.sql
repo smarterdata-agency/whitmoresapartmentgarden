7e2869067eff84432cfca9166adf24a52aca19c1
tom <87999532+whiteowlanalytics@users.noreply.github.com>
2022-03-11 11:09:33 -0800

    Update map_fb_ads_insights_creative.sql

--- models/map_fb_ads_insights_creative.sql
+++ models/map_fb_ads_insights_creative.sql
@@ -22,7 +22,7 @@ action,
 actions
 
 FROM
-`smarter-data-agency-pipelin.{{target}}.map_ads_insights_platform_proc`
+`{{target.project}}.map_ads_insights_platform_proc`
 LEFT  JOIN
-`smarter-data-agency-pipelin.{{target}}.map_facebook_creative_proc`
+`{{target.project}}.map_facebook_creative_proc`
 ON ad_id=id
