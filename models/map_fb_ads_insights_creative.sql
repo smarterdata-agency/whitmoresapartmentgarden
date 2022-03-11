07e61b962fb18c7894ae0a0cd15885b96d593d23
tom <87999532+whiteowlanalytics@users.noreply.github.com>
2022-03-11 11:05:37 -0800

    Create map_fb_ads_insights_creative.sql

---
+++ models/map_fb_ads_insights_creative.sql
@@ -0,0 +1,28 @@
+SELECT  
+
+date,
+objective,
+campaign_name,
+adset_name,
+ad_name,
+campaign_id,
+adset_id,
+ad_id,
+body,
+image_url,
+spend,
+frequency,
+cpc,
+cpm,
+reach,
+impressions,
+##engagement_rate_ranking,
+inline_link_clicks,
+action,
+actions
+
+FROM
+`{{target}}.map_ads_insights_platform_proc`
+LEFT  JOIN
+`{{target}}.map_facebook_creative_proc`
+ON ad_id=id
