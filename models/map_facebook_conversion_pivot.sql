9338770893dea1e00576e5b93ccdd2b040f5580c
tom <101282556+smarterdata-agency@users.noreply.github.com>
2022-03-14 14:18:15 -0700

    Update map_facebook_conversion_pivot.sql

--- models/map_facebook_conversion_pivot.sql
+++ models/map_facebook_conversion_pivot.sql
@@ -11,7 +11,7 @@ ad_name,
 action,
 sum(action_values) as action_values
 FROM {{ ref('map_ads_insights_conversions_proc') }}
-GROUP BY date, campaign_id, adset_id, ad_id, action
+GROUP BY date, campaign_id, adset_id, ad_id, action, campaign_name, adset_name, ad_name
 )
 PIVOT
 (
