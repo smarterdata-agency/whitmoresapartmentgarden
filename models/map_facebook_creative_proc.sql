f304e89e9d07936c1c566f362de8537ac431dedd
tom <101282556+smarterdata-agency@users.noreply.github.com>
2022-03-14 08:48:56 -0700

    Update map_facebook_creative_proc.sql

--- models/map_facebook_creative_proc.sql
+++ models/map_facebook_creative_proc.sql
@@ -3,8 +3,8 @@ SELECT
 id,
 body,
 image_url,
-object_id,
 object_story_id,
-object_story_spec
+object_story_specSELECT id as ads_table_id , creative.id as ads_table_creative_id FROM `stitch007.fb_wag.ads`
+
 
  FROM `stitch007.fb_wag.adcreative` 
