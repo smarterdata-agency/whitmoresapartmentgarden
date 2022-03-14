a258fe1888be3587e3a50b50c4ead86dc4eea256
tom <101282556+smarterdata-agency@users.noreply.github.com>
2022-03-14 14:45:30 -0700

    Update map_fb_ads_insights_creative_join.sql

--- models/map_fb_ads_insights_creative_join.sql
+++ models/map_fb_ads_insights_creative_join.sql
@@ -1,13 +1,13 @@
 SELECT  
 
-date,
+a.date,
 objective,
-campaign_name,
-adset_name,
-ad_name,
-campaign_id,
-adset_id,
-ad_id,
+a.campaign_name,
+a.adset_name,
+a.ad_name,
+a.campaign_id,
+a.adset_id,
+a.ad_id,
 ads_table_id,
 body,
 image_url,
@@ -18,22 +18,30 @@ cpm,
 reach,
 impressions,
 inline_link_clicks,
-action,
-actions
+action_values_post_reaction,
+action_values_page_engagement,
+action_values_post_engagement
 
 
 
-FROM {{ ref('map_ads_insights_platform_proc') }}
-
-
+FROM {{ ref('map_ads_insights_platform_no_conv_proc') }} a
 LEFT JOIN 
-{{ ref('map_ads_id_creative_id_proc') }} 
+{{ ref('map_ads_id_creative_id_proc') }}  b
 
 ON ad_id=ads_table_id
 
 LEFT JOIN
 
-{{ ref('map_facebook_creative_proc') }} 
+{{ ref('map_facebook_creative_proc') }}  c
 ON creative_id=ads_table_creative_id
+
+LEFT JOIN {{ ref('map_facebook_conversion_pivot') }}  d
+
+ON (a.campaign_id=d.campaign_id and a.ad_id=d.ad_id and a.date=d.date)
+
+
+
+
+
 
 
